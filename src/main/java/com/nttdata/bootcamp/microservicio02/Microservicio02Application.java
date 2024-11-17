b568222d45e6e2694c60dfa4787bf97933d5acf9
Edward Joseph Valladares Tavera <evalladarestav@gmail.com>
2024-11-16 21:19:38 -0500

    Implemented logback

--- src/main/java/com/nttdata/bootcamp/microservicio02/Microservicio02Application.java
+++ src/main/java/com/nttdata/bootcamp/microservicio02/Microservicio02Application.java
@@ -1,5 +1,7 @@
 package com.nttdata.bootcamp.microservicio02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -7,6 +9,8 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @EnableReactiveMongoRepositories
 @SpringBootApplication
 public class Microservicio02Application {
+
+  private static final Logger log = LoggerFactory.getLogger(Microservicio02Application.class);
 
   public static void main(String[] args) {
     SpringApplication.run(Microservicio02Application.class, args);
